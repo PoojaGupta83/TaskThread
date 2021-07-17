bc5ca6122a213bff2609b4ad4d0360c4d453fbd2
PoojaGupta83 <56184524+PoojaGupta83@users.noreply.github.com>
2021-07-17 23:42:38 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,44 +1,15 @@
-package Threadtask;
+package NewTask;
+
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Test {
 
 	public static void main(String[] args) {
-		CP c = new CP();
-		Publisher p1 = new Publisher(c);
-		Consumer c1 = new Consumer(c);
-		p1.start();
-		c1.start();
-	}
-}
-
-class CP {
-
-	private boolean available = false;
-
-	public synchronized void get() {
-		while (available == false) {
-			try {
-				wait();
-			} catch (Exception e) {
-				System.out.println(e);
-			}
-		}
-
-		available = false;
-		notify();
-
-	}
-
-	public synchronized void put() {
-		while (available == true) {
-			try {
-				wait();
-			} catch (Exception e) {
-			}
-		}
-
-		available = true;
-		notify();
-
+		Queue<Character> queue = new LinkedList<Character>();
+		Thread t1 = new Thread(new Publisher(queue));
+		Thread t2 = new Thread(new Consumer(queue));
+		t1.start();
+		t2.start();
 	}
 }
