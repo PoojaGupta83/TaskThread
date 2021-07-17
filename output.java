e93c10d6b725588d674a8f5d67e9841432b971b2
PoojaGupta83 <56184524+PoojaGupta83@users.noreply.github.com>
2021-07-17 23:43:28 +0530

    Update output.java

--- output.java
+++ output.java
@@ -1,6 +1,9 @@
+
+Consumer thread is empty waiting for publisher to put value
 Publisher Thread # put value into : Queue=[H, e, l, l, o]
-Consumer Thread # remove value H from: Queue=[e, l, l, o]
-Consumer Thread # remove value e from: Queue=[l, l, o]
-Consumer Thread # remove value l from: Queue=[l, o]
-Consumer Thread # remove value l from: Queue=[o]
-Consumer Thread # remove value o from: Queue=[]
+Publisher thread is full waiting for consumer to retrieve
+Consumer Thread  remove value H from: Queue=[e, l, l, o]
+Consumer Thread  remove value e from: Queue=[l, l, o]
+Consumer Thread  remove value l from: Queue=[l, o]
+Consumer Thread  remove value l from: Queue=[o]
+Consumer Thread  remove value o from: Queue=[]
